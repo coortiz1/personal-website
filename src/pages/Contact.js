b2bdccdeef7cf7286e578fc1223a45ea1f7b3eac
Curtis Chen <curtischen1234@gmail.com>
2023-07-29 14:29:36 -0700

    Add AP and SAT score details

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,11 +7,12 @@ function Contact(){
             <>
                 <h1>Reach out to me!</h1>
                 <h3>UCLA 25' Computer Science Major</h3>
-                <p>Open for opportunities</p>
+                <p>Open for opportunities!</p>
                 <ul>
                     <li>Email: curtischen1234@gmail.com</li>
                     <li>Phone Number: 1-626-321-0852</li>
                 </ul>
+                <a href = "https://www.linkedin.com/in/curtis-chen1/">Linkedin</a>
             </>
 
         );
